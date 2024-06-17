05c6d1b4307eb2381f7085a629d83866ab01a69e
Rodrigo Silva <rdg.hora1@gmail.com>
2024-06-17 13:03:12 -0300

    Initial

---
+++ 04-greeting-service/src/main/java/org/example/greetingservice/GreetingService.java
@@ -0,0 +1,13 @@
+package org.example.greetingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingService {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GreetingService.class, args);
+  }
+
+}
